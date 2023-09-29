803078eb402c19fd1eaad795ead5736a913ebf4e
MSreeN <khyathimnsree369@gmail.com>
2023-09-29 18:02:25 +0530

    xml call

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// https://countries-api-836d.onrender.com/countries/
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v2/name/India");
+request.send();
+
+request.addEventListener("load", function (e) {
+  const [, data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = `<article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>POP ${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+});

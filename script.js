3d5ae2c3dcb45f1b9ee8e7cbe2bf2c399d739e63
MSreeN <khyathimnsree369@gmail.com>
2023-09-30 11:39:56 +0530

    first fetch api

--- script.js
+++ script.js
@@ -43,13 +43,13 @@ function getCountryAndNeighbor(country) {
 
     //request for country 2
     const request2 = new XMLHttpRequest();
-    const [, , code] = data.borders;
+    const [code] = data.borders;
     request2.open("GET", `https://restcountries.com/v3.1/alpha/${code}`);
     request2.send();
     request2.addEventListener("load", function (e) {
       const [data] = JSON.parse(this.responseText);
       console.log(data);
-      renderCountry(data);
+      // renderCountry(data);
     });
   });
 }
@@ -60,7 +60,7 @@ function renderCountry(data) {
   const html = `<article class="country">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
+      <h3 class="country__name">${data.name}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>POP ${data.population}</p>
       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
@@ -71,3 +71,8 @@ function renderCountry(data) {
   countriesContainer.insertAdjacentHTML("beforeend", html);
   countriesContainer.style.opacity = 1;
 }
+
+////////////Fetch api
+
+const request = fetch("https://restcountries.com/v2/name/india");
+console.log(request);

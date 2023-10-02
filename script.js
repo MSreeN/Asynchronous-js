593749489cd6dbd3cd201a458e1b9f1b482b8223
MSreeN <khyathimnsree369@gmail.com>
2023-10-02 17:25:46 +0530

    handling error using Error

--- script.js
+++ script.js
@@ -5,6 +5,11 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+function renderError(msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+}
+
 ///////////////////////////////////////
 // function getCountryData(country) {
 //   const request = new XMLHttpRequest();
@@ -63,7 +68,9 @@ function renderCountry(data) {
       <h3 class="country__name">${data.name}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>POP ${data.population}</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.keys(data.languages)[0]
+      }</p>
       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
     </div>
   </article>`;
@@ -90,6 +97,7 @@ function renderCountry(data) {
 function getCountryData(country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then((response) => {
+      if (!response.ok) throw new Error(`Country not found ${response.status}`);
       return response.json();
     })
     .then((data) => {
@@ -105,7 +113,12 @@ function getCountryData(country) {
       const [countryData] = data;
       console.log(countryData);
       renderCountry(countryData);
-    });
+    })
+    .catch((err) => {
+      console.error(err);
+      renderError(err.message);
+    })
+    .finally((data) => console.log(data));
 }
 
-getCountryData("india");
+getCountryData("intra");
